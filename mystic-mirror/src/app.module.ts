2cacd3c9f80856f31cc02da88df9ea2b8790883a
Hojin Kim <miu.null@gmail.com>
2025-01-18 00:44:39 +0900

    root edit

--- mystic-mirror/src/app.module.ts
+++ mystic-mirror/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend', 'build'),
+      exclude: ['*/api'],
+    }),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
