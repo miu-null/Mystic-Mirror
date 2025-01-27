a66c1934050af4cff97a53463643f090fbad1810
Hojin Kim <miu.null@gmail.com>
2025-01-27 16:58:17 +0900

    edited 'reportweb-vitals file

--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,15 +1,17 @@
-import { onCLS, onFCP, onLCP, onTTFB } from 'web-vitals';
+// import {onCLS, onFCP, onLCP, onTTFB} from "web-vitals";
 
-type ReportHandler = (metric: { name: string; value: number }) => void;
+type ReportHandler = (metric: {name: string; value: number}) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && typeof onPerfEntry === 'function') {
-    onCLS(onPerfEntry);
-    // onFID(onPerfEntry);
-    onFCP(onPerfEntry);
-    onLCP(onPerfEntry);
-    onTTFB(onPerfEntry);
-  }
+    if (onPerfEntry && typeof onPerfEntry === "function") {
+        import("web-vitals").then(({onCLS, onFCP, onLCP, onTTFB}) => {
+            onCLS(onPerfEntry);
+            // onFID(onPerfEntry);
+            onFCP(onPerfEntry);
+            onLCP(onPerfEntry);
+            onTTFB(onPerfEntry);
+        });
+    }
 };
 
 export default reportWebVitals;
