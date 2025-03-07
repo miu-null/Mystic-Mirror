3e908179eb8828ca88d35be6826477f48c22b189
Hojin Kim <miu.null@gmail.com>
2025-03-07 12:39:00 +0900

    frontend root edit

--- mystic-mirror/src/app.module.ts
+++ mystic-mirror/src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'frontend', 'build'),
+      rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
       exclude: ['*/api'],
     }),
     ConfigModule.forRoot({
